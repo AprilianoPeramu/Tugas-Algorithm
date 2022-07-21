3d223a8e0e6484c32909397a60b9683fd26996b5
AprilianoPeramu <gaming.gorse@gmail.com>
2022-07-21 21:52:42 +0800

    tugas-algo

---
+++ sanbox.js
@@ -0,0 +1,28 @@
+// project 1
+function convert(jam){
+    return 3600 * jam
+  }console.log(convert(2))
+  
+  //project 2
+  let num1 = 90
+  let num2 = 90
+  if (num1 >num2){
+    console.log(num1 +" yang besar")
+  }else if (num1 === num2){
+  console.log(num1 +" nialainya sama")
+  } else  {
+    console.log(num2 +" yang besar")
+  } 
+  // project 3 
+  let jumlahTreveler = 2
+  let hargaTiket = 600.000
+  let totalHarga
+  totalHarga = jumlahTreveler * hargaTiket
+  console.log(totalHarga)
+  
+  // project 4
+  
+  let nilai = 100
+  for(let i = 0; i <= nilai; i++){
+    console(i)
+}
